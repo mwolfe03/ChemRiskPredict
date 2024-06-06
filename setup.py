ba4282bc0f5e063df742ae884d8bf919dd077a4c
mwolfe03 <168668591+mwolfe03@users.noreply.github.com>
2024-06-05 19:42:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ NAME = 'ChemRiskPredict'
 VERSION = '1.0.0'
 DESCRIPTION = 'A package for predicting chemical hazards.'
 AUTHOR = 'Mateo Wolfe'
-EMAIL = 'mwolfe08@calpoly.edu, mateowolf3@gmail.com'
+EMAIL = 'mwolfe08@calpoly.edu'
 URL = 'https://github.com/mwolfe03/ChemRiskPredict'
 LICENSE = 'MIT'
 KEYWORDS = ['chemistry', 'risk prediction', 'hazard prediction, cheminformatics']
@@ -23,7 +23,7 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description= "Predicts varius hazards from Canonical SMILES structures",
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=EMAIL,
