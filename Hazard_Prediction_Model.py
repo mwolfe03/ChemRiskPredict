5543efbb7fe61b6154221311acc2adea31309f75
mwolfe03 <168668591+mwolfe03@users.noreply.github.com>
2024-06-20 00:11:00 -0700

    added more hazards

--- Hazard_Prediction_Model.py
+++ Hazard_Prediction_Model.py
@@ -88,7 +88,7 @@ def pull_hazards_from_dataframe(index_list: list, main_df: pd.DataFrame,
     if hazard_list is None:
 
         hazard_list = ["AcuteToxic", "Flammable", "HealthHazard", "Corrosive",
-                       "EnvironmentalHazard"]
+                       "EnvironmentalHazard", "Irritant", "CompressedGas", "Oxidizer", "Explosive", "CompressedGas"]
 
     filtered_df = main_df.iloc[index_list]
     len_index_list = len(index_list) # this is the number of neighbors we are using
